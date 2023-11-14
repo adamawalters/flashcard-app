a4b5fa6c5bdd1bbd45bfb9c44459d042decc784d
adamawalters <adamawalters@gmail.com>
2023-11-13 19:28:01 -0500

    updated theming

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 function Header() {
   return (
-    <header className="jumbotron bg-dark">
+    <header className="jumbotron" >
       <div className="container text-white">
         <h1 className="display-4"><Link to="/" className="text-reset">FlipFlash</Link></h1> 
         <p className="lead">Create and study flashcards</p>
